2b76e7ed8bf554e7a6321276f3f71b9bbb97a186
Chris Loew <cloew123@gmail.com>
2016-06-23 17:24:48 -0600

    Removed package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(name='kao_symbols',
       version='0.0.1',
       description="",
-      author='',
-      author_email='',
-      packages=['kao_symbols'],
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      py_modules=['kao_symbols'],
      )
