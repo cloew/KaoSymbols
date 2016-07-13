fcdfbfc568896d7133a16187867a2053f0f5ff00
Chris Loew <cloew123@gmail.com>
2016-07-12 18:27:55 -0600

    Can now specify an order to convert symbols in

--- kao_symbols.py
+++ kao_symbols.py
@@ -1,6 +1,16 @@
 
 def convert(text, mappings):
     """ Convert the text using the mapping given """
-    for key, value in mappings.items():
+    if hasattr(mappings, 'items'):
+        return _convert(text, mappings)
+    else:        
+        for mapping in mappings:
+            text = _convert(text, mapping)
+        return text
+    
+
+def _convert(text, mapping):
+    """ Convert the text using the mapping given """
+    for key, value in mapping.items():
         text = text.replace(key, value)
     return text
