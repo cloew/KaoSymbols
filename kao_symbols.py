223391e943e729535e1e2444620c32a8eea85e4f
Chris Loew <cloew123@gmail.com>
2016-07-12 18:24:49 -0600

    Allowed using string keys rather than single character keys

--- kao_symbols.py
+++ kao_symbols.py
@@ -1,14 +1,6 @@
 
 def convert(text, mappings):
     """ Convert the text using the mapping given """
-    pieces = []
-    needToRunAgain = False
-    for c in text:
-        if c in mappings:
-            pieces.append(mappings[c])
-            needToRunAgain = True
-        else:
-            pieces.append(c)
-    if needToRunAgain:
-        pieces = convert(pieces, mappings)
-    return "".join(pieces)
+    for key, value in mappings.items():
+        text = text.replace(key, value)
+    return text
