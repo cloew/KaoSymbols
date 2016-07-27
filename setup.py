76868c9e125b7844e3c848c929dbe83db5b25983
Chris Loew <cloew123@gmail.com>
2016-07-27 17:34:58 -0600

    Version Bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='kao_symbols',
-      version='0.1.0',
+      version='0.1.1',
       description="",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
